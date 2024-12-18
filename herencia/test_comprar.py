8beffd9f68286f02f3998feb062f6dd98968ac1f
Raysder0 <pedro05yoss@gmail.com>
2024-12-17 22:20:10 -0500

    agregar al carro de compras

--- herencia/test_comprar.py
+++ herencia/test_comprar.py
@@ -12,9 +12,20 @@ def setup():
     yield driver
     driver.quit()
 
+def name_object(driver):
+    wait = WebDriverWait(driver, 10)
+    nameObject = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='item_0_title_link']")))
+    nameObject.click()
+
+def add_car(driver):
+    wait = WebDriverWait(driver, 10)
+    add_to_cart = wait.until(EC.element_to_be_clickable((By.ID, "add-to-cart")))
+    add_to_cart.click()
+
 def test_login(setup):
     driver = setup
     driver.get("https://www.saucedemo.com/")
+
     login(driver, "standard_user", "secret_sauce")
-    
-    assert "inventory.html" in driver.current_url, "Login failed"
+    name_object(driver)
+    add_car(driver)
